3c126eb35edf17e76976cf3360c24b2270cb6446
Xervan-0 <anonpin45@gmail.com>
2025-07-29 15:10:04 +0800

    update

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -1,16 +1,12 @@
 const images = [
   {
-    src: "Shark-1.jpg",
+    src: "illustrations/Shark-1.jpg",
     caption: "An editorial cartoon that powerfully illustrates the threat of illegal fishing to marine ecosystems, highlighting its destructive impact on shark habitats and the broader oceanic environment."
   },
   {
-    src: "France-1.jpg",
+    src: "illustrations/France-1.jpg",
     caption: "This cartoon symbolizes France's active and urgent effort to deliver humanitarian or military aid to the Philippines, likely in response to a crisis, disaster, or conflict."
               + "The size of the crate and the focused trajectory underscore the magnitude and intent behind the assistance—depicting it as impactful and essential."
-  },
-  {
-    src: "Weiner.jpg",
-    caption: "Editorial art showing the ripple effect of illegal shark finning."
   }
 ];
 
